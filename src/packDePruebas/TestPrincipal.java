175a1fcb2a29c37aa93a6ebd4b573aa412fb1258
JonnathanJuarez <jua15377@uvg.edu.gt>
2018-03-19 23:20:34 -0600

    se agregan las primeras etapas de la gramtica

--- src/packDePruebas/TestPrincipal.java
+++ src/packDePruebas/TestPrincipal.java
@@ -1,11 +1,12 @@
 package packDePruebas;
 
-import
+
+
 public class TestPrincipal
 
 {
     public  static void main(String args[]){
         System.out.print("Heelo DBM!");
-        FileMan
+
     }
 }
