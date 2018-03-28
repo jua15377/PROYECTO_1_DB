e900afb8a61d68d6584d7a11f07107c7b891a584
CoffeeBrewer <cas15151@uvg.edu.gt>
2018-03-28 15:58:29 -0600

    Cambios en Clases :D

--- src/fileManagement/Tabla.java
+++ src/fileManagement/Tabla.java
@@ -1,16 +1,25 @@
 package fileManagement;
 
+import javafx.scene.control.Tab;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
 public class Tabla implements Serializable{
-    private String Name;
+    private String name;
     private ArrayList<String> nombresDecolumnas = new ArrayList<>();
     private ArrayList<String> tiposDecolumnas = new ArrayList<>();
-    private String pk = "";
-    private String fk = "";
-    private Hashtable<Integer, Registro> refistro = new Hashtable<>();
+    private ArrayList<String> pk = new ArrayList<>();
+    private ArrayList<String> fk = new ArrayList<>();
+    private Hashtable<Integer, Registro> registro = new Hashtable<>();
+
+    public Tabla(String name){
+        this.name = name;
+
+    }
+
+
 
 
 
