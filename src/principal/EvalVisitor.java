a4e0fdc565acdd13bac983813b46c409869fc0f5
CoffeeBrewer <cas15151@uvg.edu.gt>
2018-04-01 01:56:12 -0600

    Terminando visitor STMalterDatabase

--- src/principal/EvalVisitor.java
+++ src/principal/EvalVisitor.java
@@ -2,6 +2,8 @@ package principal;
 
 import antlrGenerateFiles.*;
 import fileManagement.*;
+
+import java.beans.Visibility;
 import java.util.ArrayList;
 
 public class EvalVisitor extends PostSQLBaseVisitor<String>{
@@ -51,6 +53,39 @@ public class EvalVisitor extends PostSQLBaseVisitor<String>{
         if(verboseEnable){ verbose += "Base de Datos: " + id + ", eliminada con exito\n";}
         return visitChildren(ctx);
     }
+
+    /***
+     * Grammar: ALTER DATABASE ID RENAME TO ID*/
+    @Override public String visitSTMalterDB(PostSQLParser.STMalterDBContext ctx) {
+        String idViejo = ctx.ID(0).getText();
+        String idNuevo = ctx.ID(1).getText();
+
+        if (!manejador.getDbsNames().contains(idViejo)){
+            //ERROR: No existe la base de datos inicial
+            return error = "Error en la linea:" + ctx.getStart().getLine()+", "+ ctx.getStart().getCharPositionInLine()+
+                    ". La base de datos " + ctx.ID(0).getText()+" no existe.\n";
+        }
+
+        else if(manejador.getDbsNames().contains(idNuevo)){
+            //Error
+            return error = "Error en la linea:" + ctx.getStart().getLine()+", "+ ctx.getStart().getCharPositionInLine()+
+                    ". El nombre  " + ctx.ID(1).getText()+" ya existe .\n";
+        }
+
+        else{
+            manejador.changeDBSname(idViejo, idNuevo);
+            if(verboseEnable){
+                return verbose += "Base de Datos: " + ctx.ID(0).getText() + " renombrada a " + ctx.ID(1).getText() +
+                        "con exito\n";
+            }
+        }
+
+        return visitChildren(ctx);
+
+
+    }
+
+
     @Override public String visitSTMcreateTable(PostSQLParser.STMcreateTableContext ctx) {
 
         return visitChildren(ctx);
